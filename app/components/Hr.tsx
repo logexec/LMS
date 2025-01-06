037f82c962511033fd52d9fc13d336b19bcf1a50
rick-dev-logex <ricardo.estrella@logex.ec>
2025-01-06 15:36:16 -0500

    Added Hr custom component

---
+++ app/components/Hr.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Hr: React.FC = () => {
+  return (
+    <div className="h-[1px] w-11/12 bg-gradient-to-r from-slate-400 to-transparent" />
+  );
+};
+
+export default Hr;
