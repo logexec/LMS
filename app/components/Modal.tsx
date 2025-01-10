6c7a5b4acbb8b461eb71293c9a3f44d3898cbc44
rick-dev-logex <ricardo.estrella@logex.ec>
2025-01-10 10:18:11 -0500

    updated styles

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -11,7 +11,7 @@ export const Modal: React.FC<ModalProps> = ({ isOpen, children }) => {
 
   return createPortal(
     <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50 flex items-center justify-center p-4">
-      <div className="bg-white rounded-lg shadow-xl max-w-md w-full max-h-[90vh] overflow-auto">
+      <div className="bg-white rounded-lg shadow-xl max-w-[50rem] max-h-[90vh] overflow-auto">
         <div className="p-6">{children}</div>
       </div>
     </div>,
