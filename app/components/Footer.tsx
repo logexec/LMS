e1a996129925e3280b87a857db9ad4bf2c5a40ad
rick-dev-logex <ricardo.estrella@logex.ec>
2025-01-14 15:03:07 -0500

    updated footer content

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-import Link from "next/link";
-import { IoLocationOutline } from "react-icons/io5";
+import React from "react";
 
-const Footer = () => {
+const Footer: React.FC = () => {
   const months = [
     "Enero",
     "Febrero",
@@ -26,46 +24,6 @@ const Footer = () => {
     "Viernes",
     "Sábado",
   ];
-
-  const isAuthenticated: boolean = true;
-
-  if (!isAuthenticated) {
-    return (
-      <footer className="row-start-3 flex gap-6 p-2 flex-wrap items-center justify-evenly w-full bg-black text-white">
-        <Link href="/">
-          <Image
-            aria-hidden
-            src="/images/logo_transparent.png"
-            alt="LogeX logo"
-            width={180}
-            height={38}
-          />
-        </Link>
-        <div className="flex items-center flex-wrap max-w-72 text-sm">
-          <div className="flex flex-row gap-2">
-            <IoLocationOutline size={20} />
-            <div className="flex flex-col">
-              <h2 className="font-semibold">Quito</h2>
-              <p className="font-light">
-                Av. República del Salvador E9-10 y Av. Shyris, Edif. Onix, PH.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center flex-wrap max-w-72 text-sm">
-          <div className="flex flex-row gap-2">
-            <IoLocationOutline size={20} />
-            <div className="flex flex-col">
-              <h2 className="font-semibold">Guayaquil</h2>
-              <p className="font-light">
-                Av. Juan Tanca Marnego y Ma. Piedad Castillo de Levi.
-              </p>
-            </div>
-          </div>
-        </div>
-      </footer>
-    );
-  }
 
   return (
     <footer className="row-start-3 flex gap-6 p-2 flex-wrap items-center justify-evenly w-full bg-black text-white">
