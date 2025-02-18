581f0d36bedc8b17e65e5fb6f0d379f0473ebec9
rick-dev-logex <ricardo.estrella@logex.ec>
2025-02-18 17:29:10 -0500

    updated transition colors

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -43,7 +43,7 @@ const LoginPage = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className="min-h-screen flex flex-col lg:flex-row overflow-hidden"
+      className="min-h-screen flex flex-col lg:flex-row overflow-hidden transition-colors duration-300"
     >
       <motion.span
         initial={{ opacity: 0, top: 50 }}
