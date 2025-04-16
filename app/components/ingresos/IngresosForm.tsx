f09e506c83ccb544ddcd90a6a5751db7f6ae4c9a
rick-dev-logex <ricardo.estrella@logex.ec>
2025-04-16 16:58:58 -0500

    disabled hooks deps rule

--- app/components/ingresos/IngresosForm.tsx
+++ app/components/ingresos/IngresosForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useMemo } from "react";
 import { motion, AnimatePresence } from "motion/react";
 import { toast } from "sonner";
