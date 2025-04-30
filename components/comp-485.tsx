34f65cb45cd255839e8a3ae3841e1fb833aed78f
rick-dev-logex <ricardo.estrella@logex.ec>
2025-04-30 14:43:39 -0500

    fix new table

--- components/comp-485.tsx
+++ components/comp-485.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import { useEffect, useId, useMemo, useRef, useState } from "react";
