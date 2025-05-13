1d9375dbac1a5a7da40217d95a1a9ca73ce3e53d
rick-dev-logex <ricardo.estrella@logex.ec>
2025-05-13 08:45:17 -0500

    added edit request dialog component

---
+++ app/registros/components/EditRequestDialog.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// interface EditRequestComponentProps {
+//     unique_id: string;
+// }
+
+// interface EditRequestProps {
+//     unique_id: string;
+// }
+
+const EditRequestDialog = () => {
+  return <div></div>;
+};
+
+export default EditRequestDialog;
